d87a55eba7c1e72759eb65cf752aaeac605aa18d
RomanMakoviichuk <puzzlemetalcore@gmail.com>
2025-08-12 09:34:02 +0300

    повноекранний режим

--- src/pages/StartMenu.jsx
+++ src/pages/StartMenu.jsx
@@ -2,11 +2,28 @@ import React from "react";
 import './StartMenu.css';
 
 export default function StartMenu({ onNewGame, onExit }) {
+    const openFullscreen = () => {
+        const elem = document.documentElement;
+        if (elem.requestFullscreen) {
+            elem.requestFullscreen().catch(() => {}); // на випадок помилки
+        } else if (elem.webkitRequestFullscreen) {
+            elem.webkitRequestFullscreen();
+        } else if (elem.msRequestFullscreen) {
+            elem.msRequestFullscreen();
+        }
+    };
+
+    const handleNewGame = () => {
+        openFullscreen();
+        onNewGame();
+    };
+
     return (
-        <div className="startMenu">
-            <h1>Гра <span>«Шпигун»</span></h1>
-            <button className="main_btn" onClick={onNewGame}>Створити нову гру</button>
+        <div className="start-menu">
+            <h1>Гра «Шпигун»</h1>
+            <button onClick={handleNewGame}>Створити нову гру</button>
             <button onClick={onExit}>Вийти з гри</button>
         </div>
     );
 }
+
