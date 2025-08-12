822eecc463c6c9280205cd5cec79d77364340f27
RomanMakoviichuk <puzzlemetalcore@gmail.com>
2025-08-12 09:45:04 +0300

    повноекранний режим PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,34 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from "vite-plugin-pwa";
+
 
 export default defineConfig({
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: "autoUpdate",
+            manifest: {
+                name: "Гра Шпигун",
+                short_name: "Шпигун",
+                start_url: "./index.html",
+                display: "standalone",
+                background_color: "#000000",
+                theme_color: "#333333",
+                icons: [
+                    {
+                        src: "icons/icon-192.png",
+                        sizes: "192x192",
+                        type: "image/png"
+                    },
+                    {
+                        src: "icons/icon-512.png",
+                        sizes: "512x512",
+                        type: "image/png"
+                    }
+                ]
+            }
+        })
+    ],
     base: '/SpyGame/',
-    plugins: [react()]
 });
