3fedb257c6372f34ae62d09e3835fe67e83b3436
RomanMakoviichuk <puzzlemetalcore@gmail.com>
2025-08-12 11:10:24 +0300

    spy count

--- src/pages/GameSetup.jsx
+++ src/pages/GameSetup.jsx
@@ -48,19 +48,16 @@ export default function GameSetup({
 
             <label>
                 Кількість шпигунів:
-                <input
-                    type="number"
-                    min="1"
-                    max={players.length - 1}
+                <select
                     value={spiesCount}
-                    onChange={(e) =>
-                        setSpiesCount(
-                            Math.min(Math.max(1, Number(e.target.value)), players.length - 1)
-                        )
-                    }
-                />
+                    onChange={(e) => setSpiesCount(Number(e.target.value))}
+                >
+                    <option value={1}>1 шпигун</option>
+                    <option value={2} disabled={players.length < 3}>2 шпигуна</option>
+                    <option value={3} disabled={players.length < 4}>3 шпигуна</option>
+                </select>
             </label>
-            <br />
+            <br/>
             <button
                 onClick={() => players.length > 1 && spiesCount < players.length && onNext()}
                 disabled={players.length < 2 || spiesCount >= players.length}
